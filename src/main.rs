e338df32a52b21ac38ff3c2c29ee88243119ccea
Alex Kahn <alex.kahn@netlify.com>
2023-01-28 16:26:15 -0500

    Better CLI

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 use std::net::{TcpStream};
-use std::process::exit;
 use std::time::Duration;
 use std::net::ToSocketAddrs;
+use clap::Parser;
 
 fn main() -> std::io::Result<()>{
-    let d = Duration::new(1, 0);
-    let hostname = match std::env::args().nth(1) {
-        Some(address) => address,
-        None => {
-            println!("no address provided");
-            exit(1)
-        }
-    };
+    let args = Cli::parse();
+    let d = Duration::new(args.timeout as u64, 0);
+    let hostname = args.host;
 
     println!("Scanning {}", hostname);
 
-    let min_port = std::env::args().nth(2).
-        and_then(|t| {Some(t.parse::<u32>().expect("invalid min port"))}).
-        unwrap_or(1);
-
-    let max_port = std::env::args().nth(3).
-        and_then(|t| {Some(t.parse::<u32>().expect("invalid max port"))}).
-        unwrap_or(1);
-
-    let ports = min_port..(max_port+1);
+    let ports = args.min_port..(args.max_port+1);
 
     for i in ports {
         let address = hostname.clone() + ":" + &i.to_string();
@@ -43,13 +30,20 @@ fn main() -> std::io::Result<()>{
         }
     }
 
-
-    // Read address from command line
-
-    // Range over ports 0..10000, or read min/max ports from CLI args
-
-    // Try to open a TCP connection, only wait ~1 second
-
-
     Ok(())
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(long)]
+    host: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    timeout: u8,
+
+    #[arg(long, default_value_t = 1)]
+    min_port: u16,
+
+    #[arg(long, default_value_t = 1)]
+    max_port: u16
 }
